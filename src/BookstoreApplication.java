53c0b1460d075839b408f2c1cfaab257fc90856f
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-28 21:25:58 +0300

    is rdy

--- src/BookstoreApplication.java
+++ src/BookstoreApplication.java
@@ -2,6 +2,7 @@ public class BookstoreApplication {
     public static void main(String[] args) throws BookNotFoundException {
         Bookstore bookstore = new Bookstore();
         bookstore.addBook(new Book("It ends with Us", "Colleen Hoover", 15.99));
+        bookstore.addBook(new Book("It ends with Us", "Colleen Hoover", 15.99));
         bookstore.addBook(new Book("Confess", "Colleen Hoover", 12.00));
         bookstore.addBook(new Book("Maybe Someday", "Colleen Hoover", 10.99));
 
@@ -12,8 +13,10 @@ public class BookstoreApplication {
             System.out.println(e.getMessage());
         }
         try {
-            bookstore.sellBook("Confess", 2);
-            bookstore.sellBook("Maybe Someday", 2);
+            bookstore.sellBook("Confess", 1);
+            bookstore.sellBook("Maybe Someday", 1);
+            bookstore.sellBook("It end with Us", 1);
+
         } catch (InsufficientStockException | BookNotFoundException e) {
             System.out.println(e.getMessage());
         }
