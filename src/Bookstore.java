198d45a4c96db88829320a6513d59beaef4e0e29
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-29 18:50:39 +0300

    is rdy

--- src/Bookstore.java
+++ src/Bookstore.java
@@ -15,8 +15,9 @@ public class Bookstore {
         if (books.containsKey(book)) {
             books.put(book, books.get(book) + 1);
 
+        } else {
+            books.put(book, 1);
         }
-        books.put(book, 1);
     }
 
     public void addMultipleBooks(Book book, int quantity) {
@@ -24,20 +25,20 @@ public class Bookstore {
     }
 
     public Book findBook(String title) throws BookNotFoundException {
-        for (Map.Entry<Book, Integer> entry : books.entrySet()) {
-            Book book = entry.getKey();
+
+        for (Book book : books.keySet()) {
             if (book.getTitle().equalsIgnoreCase(title)) {
                 return book;
             }
         }
 
-        throw new BookNotFoundException("The book is not found!");
+        throw new BookNotFoundException("The book is not found! ");
     }
 
     public void displayBooks() {
         System.out.println("The books in the bookstore : ");
         for (Map.Entry<Book, Integer> entry : books.entrySet()) {
-            System.out.println("Book " + entry.getKey() + ". Quantity : " + entry.getValue() + ".");
+            System.out.println(entry.getKey() + " " + entry.getValue() + " pcs");
         }
 
 
@@ -45,12 +46,15 @@ public class Bookstore {
 
     public void sellBook(String title, int quantity) throws InsufficientStockException, BookNotFoundException {
         Book book = findBook(title);
-        int availableQ = books.getOrDefault(book, 0);
-        if (availableQ > 0 && availableQ >= quantity) {
-            books.put(book, availableQ - quantity);
-        } else {
-            throw new InsufficientStockException("Insufficient stock for book: " + title);
+        if (book == null) {
+            throw new BookNotFoundException("Nincs ilyen könyv");
         }
+
+        if (!books.containsKey(book) || books.get(book) < quantity) {
+            throw new InsufficientStockException("Nincs elég készlet a könyvből");
+        }
+
+        books.replace(book, books.get(book) - quantity);
     }
 
 }
--- src/BookstoreApplication.java
+++ src/BookstoreApplication.java
@@ -1,28 +1,30 @@
+
 public class BookstoreApplication {
-    public static void main(String[] args) throws BookNotFoundException {
+    public static void main(String[] args) {
         Bookstore bookstore = new Bookstore();
-        bookstore.addBook(new Book("It ends with Us", "Colleen Hoover", 15.99));
         bookstore.addBook(new Book("It ends with Us", "Colleen Hoover", 15.99));
         bookstore.addBook(new Book("Confess", "Colleen Hoover", 12.00));
         bookstore.addBook(new Book("Maybe Someday", "Colleen Hoover", 10.99));
 
-
         try {
-            System.out.println("The book is found : " + bookstore.findBook("l"));
+            Book book = bookstore.findBook("l");
+            if (book != null) {
+                System.out.println(book);
+            } else {
+                System.out.println("Nincs ilyen könyv");
+            }
         } catch (BookNotFoundException e) {
             System.out.println(e.getMessage());
         }
+
         try {
             bookstore.sellBook("Confess", 1);
             bookstore.sellBook("Maybe Someday", 1);
-            bookstore.sellBook("It end with Us", 1);
-
+            bookstore.sellBook("It ends with Us", 1);
         } catch (InsufficientStockException | BookNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
         bookstore.displayBooks();
-
-
     }
 }
